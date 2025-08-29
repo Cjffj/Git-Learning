901c6e1d46c5f448586f6e7e9ce5d2babbecd96d
Priyanshu Negi <priyanshunegi.409@gmail.com>
2025-08-29 17:40:30 +0530

    Payment update after footer

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
+<<<<<<< HEAD
 const footer = "Footer added in our website";
 console.log(footer);
+=======
+const payment = "Integreted the payment gateway";
+console.log(payment);
+>>>>>>> 1f22253 (Payment Added)
