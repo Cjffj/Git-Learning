3c6d21854cfb9689f370ad51fc7e402b818903d6
Priyanshu Negi <priyanshunegi.409@gmail.com>
2025-08-29 20:35:32 +0530

    Latest change

--- first.js
+++ first.js
@@ -4,16 +4,13 @@ console.log(button);
 const login = "Login page added";
 console.log(login);
 
-<<<<<<< HEAD
 const footer = "Footer added in our website";
 console.log(footer);
-=======
+
 const payment = "Integreted the payment gateway";
 console.log(payment);
-<<<<<<< HEAD
->>>>>>> 1f22253 (Payment Added)
-=======
 
 const upi = "Integrated the UPI";
 console.log(upi);
->>>>>>> 49c9fb6 (UPI Added)
+
+console.log("Latest update");
